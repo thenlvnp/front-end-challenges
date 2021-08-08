416e4aa657c760d37e116fc5aa4d253564f7be41
thenlvnp <42989819+thenlvnp@users.noreply.github.com>
2021-08-08 10:46:14 +0800

    add challenges list in landing page

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,93 @@
-import Head from 'next/head'
+import Head from "next/head";
+import fs from "fs";
+import { useEffect } from "react";
+export default function Home({ pages }) {
+    useEffect(() => {
+        console.log(`pages`, pages);
+    }, [pages]);
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <Head>
+                <title>Front End Challenges</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+            <main className="flex flex-col items-center justify-center flex-1 w-full px-20 text-center">
+                <h1 className="text-6xl font-bold">Completed Challenges</h1>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+                <p className="mt-3 text-2xl">Click on a card to view output</p>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
+                <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
+                    {pages.map((page) => (
+                        <a
+                            href={page.url}
+                            key={page.name}
+                            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+                        >
+                            <h3 className="text-2xl font-bold">
+                                {page.name} &rarr;
+                            </h3>
+                            {/* <p className="mt-4 text-xl">
+                                Find in-depth information about Next.js features
+                                and API.
+                            </p> */}
+                        </a>
+                    ))}
+                </div>
+            </main>
 
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            <footer className="flex items-center justify-center w-full h-24 border-t">
+                <a
+                    className="flex items-center justify-center"
+                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Powered by{" "}
+                    <img
+                        src="/vercel.svg"
+                        alt="Vercel Logo"
+                        className="h-4 ml-2"
+                    />
+                </a>
+            </footer>
         </div>
-      </main>
+    );
+}
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
-    </div>
-  )
+export async function getServerSideProps() {
+    const baseUrl = {
+        development: "http://localhost:3000",
+        production: "https://front-end-challenges-theta.vercel.app/",
+    }[process.env.NODE_ENV];
+
+    const staticPages = fs
+        .readdirSync("pages")
+        .filter((staticPage) => {
+            return ![
+                "_app.js",
+                "_document.js",
+                "_error.js",
+                "sitemap.xml.js",
+                "api",
+                "index.js",
+            ].includes(staticPage);
+        })
+        .map((staticPagePath) => {
+            return {
+                url: `${baseUrl}/${staticPagePath.substr(
+                    0,
+                    staticPagePath.lastIndexOf(".")
+                )}`,
+                name: staticPagePath
+                    .replace("-", " ")
+                    .substr(0, staticPagePath.lastIndexOf(".")),
+            };
+        });
+
+    return {
+        props: {
+            pages: staticPages,
+        },
+    };
 }
